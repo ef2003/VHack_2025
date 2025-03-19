41dd8ada36211710a5094c30fd75d05faaaff146
emilyfrancis2003 <emilyfrancis2003@gmail.com>
2025-03-19 10:47:34 +0800

    update

--- JAVASCRIPT/home.js
+++ JAVASCRIPT/home.js
@@ -1,18 +1,3 @@
-document.getElementById("languageToggle").addEventListener("click", function() {
-    let langText = this.querySelector(".language-text");
-    let currentLang = langText.textContent;
-    
-    if (currentLang === "EN") {
-        langText.textContent = "中文";
-    } else if (currentLang === "中文") {
-        langText.textContent = "BM";
-    } else {
-        langText.textContent = "EN";
-    }
-    
-    this.classList.toggle("active");
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const donateButtons = document.querySelectorAll(".donate-btn");
     const donationModal = new bootstrap.Modal(document.getElementById("donationModal"));
---
+++ JAVASCRIPT/navBar.js
@@ -0,0 +1,14 @@
+document.getElementById("languageToggle").addEventListener("click", function() {
+    let langText = this.querySelector(".language-text");
+    let currentLang = langText.textContent;
+    
+    if (currentLang === "EN") {
+        langText.textContent = "中文";
+    } else if (currentLang === "中文") {
+        langText.textContent = "BM";
+    } else {
+        langText.textContent = "EN";
+    }
+    
+    this.classList.toggle("active");
+});
