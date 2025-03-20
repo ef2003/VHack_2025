9989a3168b4e0e17d92eaa5371e8534a5f89ee3d
emilyfrancis2003 <emilyfrancis2003@gmail.com>
2025-03-20 23:18:13 +0800

    update donate page

--- JAVASCRIPT/donatePage.js
+++ JAVASCRIPT/donatePage.js
@@ -36,6 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Greenpeace",
             image: "../images/ad2.jpg",
             tags: ["all", "environment"],
+            amount: ["RM 500,000"],
             rating: "★★★★☆",
             description: "Preserving nature through reforestation, sustainable living initiatives, and climate change awareness. Our mission is to create a greener future by reducing pollution and protecting biodiversity."
         },
@@ -44,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Greenpeace",
             image: "../images/rainforest.jpg",
             tags: ["all", "environment"],
+            amount: ["RM 97,620"],
             rating: "★★★★★",
             description: "A conservation project focused on protecting rainforests, preventing deforestation, restoring ecosystems, and preserving wildlife for future generations."
         },
@@ -52,6 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Red Crescent Society",
             image: "../images/flood.jpeg",
             tags: ["all", "environment"],
+            amount: ["RM 265,301"],
             rating: "★★★★☆",
             description: "A community-driven initiative focused on flood preparedness, emergency response training, and post-flood recovery support to minimize disaster impact."
         },
@@ -60,6 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Green Earth Org",
             image: "../images/rainforest2.jpg",
             tags: ["all", "environment"],
+            amount: ["RM 41,860"],
             rating: "★★★★☆",
             description: "The world's rainforests are disappearing at an alarming rate due to deforestation, illegal logging, and climate change. This project focuses on preserving the remaining rainforests by funding reforestation initiatives, wildlife protection programs, and local community engagement to ensure sustainable conservation. Your donation will help protect biodiversity, absorb carbon emissions, and preserve the lungs of our planet for future generations."
         },
@@ -68,6 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Coral Reef Alliance",
             image: "../images/ad3.jpg",
             tags: ["all", "ocean conservation"],
+            amount: ["RM 300,000"],
             rating: "★★★★☆",
             description: "Restore marine ecosystems, combat ocean pollution, and protect endangered sea life. Through cleanup drives, awareness campaigns, and conservation efforts, we aim to safeguard our oceans for future generations."
         },
@@ -76,6 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "ASPCA",
             image: "../images/ad1.jpeg",
             tags: ["all", "animal rescue"],
+            amount: ["RM 102,090"],
             rating: "★★★★☆",
             description: "Rescuing, rehabilitating, and rehoming stray and endangered animals. We work to prevent animal cruelty, promote responsible pet ownership, and advocate for stronger animal protection laws."
         },
@@ -84,6 +90,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Four Paws International",
             image: "../images/rescue_pets.jpg",
             tags: ["all", "animal rescue"],
+            amount: ["RM 185,333"],
             rating: "★★★★☆",
             description: "A project dedicated to rescuing, rehabilitating, and rehoming abandoned pets, ensuring their safety, health, and a loving future."
         },
@@ -92,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "UNICEF (United Nations Children's Fund)",
             image: "../images/educate_child.jpg",
             tags: ["all", "children welfare"],
+            amount: ["RM 54,982"],
             rating: "★★★★★",
             description: "A project providing education, resources, and opportunities to underprivileged children, empowering them for a brighter future."
         },
@@ -102,6 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Future Bright",
             image: "../images/child_edu.jpg",
             tags: ["all", "children welfare"],
+            amount: ["RM 33,300"],
             rating: "★★★★☆",
             description: "Education is the key to breaking the cycle of poverty, yet millions of children worldwide lack access to basic schooling. This project provides free educational resources, school supplies, and scholarships to underprivileged children, empowering them with knowledge and skills for a brighter future. Your donation helps fund school programs, teacher training, and infrastructure improvements in disadvantaged communities."
         },
@@ -110,6 +119,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Global FoodBanking Network (GFN)",
             image: "../images/hunger_relief",
             tags: ["all", "hunger relief", "children welfare"],
+            amount: ["RM 478,122"],
             rating: "★★★★☆",
             description: "A project to eliminate hunger through sustainable food distribution, reducing waste, and ensuring access to nutritious meals for all."
         },
@@ -118,6 +128,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Global Food Aid",
             image: "../images/feed_the_hungry.jpg",
             tags: ["all", "hunger relief"],
+            amount: ["RM 576,800"],
             rating: "★★★★★",
             description: "Hunger is a daily reality for millions of people, with children and the elderly being the most vulnerable. This project distributes emergency food packs, supports local food banks, and funds sustainable agricultural initiatives to combat malnutrition. Your donation will help provide meals to families in crisis, invest in farming programs, and create long-term solutions to food insecurity."
         },
@@ -127,6 +138,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Wildlife Conservation Network",
             image: "../images/endangered.jpg",
             tags: ["all", "animal rescue"],
+            amount: ["RM 66,777"],
             rating: "★★★★☆",
             description: "Poaching, habitat destruction, and climate change are driving many species towards extinction. This initiative works to protect endangered animals by funding anti-poaching units, habitat restoration projects, and conservation education programs. Your support helps safeguard iconic species like elephants, tigers, and sea turtles, preserving them for generations to come."
         },
@@ -135,6 +147,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "World Health Organization (WHO)",
             image: "../images/thermo_health.jpg",
             tags: ["all", "health research"],
+            amount: ["RM 32,321"],
             rating: "★★★★☆",
             description: "A research project exploring thermotherapy’s impact on pain relief, healing, and overall health using advanced thermal treatment technologies."
         },
@@ -143,6 +156,7 @@ document.addEventListener("DOMContentLoaded", () => {
             organization: "Mind & Wellness Initiative",
             image: "../images/mental-health.jpg",
             tags: ["all", "health research"],
+            amount: ["RM 88,666"],
             rating: "★★★★☆",
             description: "Mental health issues affect millions, yet access to proper care and resources remains a challenge. This project funds free counseling services, crisis hotlines, and community outreach programs to promote mental well-being. Your support can make a difference in reducing stigma, providing therapy, and ensuring no one struggles alone."
         },
@@ -161,9 +175,13 @@ document.addEventListener("DOMContentLoaded", () => {
             projectElement.classList.add("project-item");
             projectElement.innerHTML = `
                 <div>
-                    <h3>${project.name}</h3>
+                    <div style="display: flex; align-items: center; gap: 10px;">
+                        <h3>${project.name}</h3>
+                        <i class="fas fa-sync-alt refresh-icon"></i>
+                    </div>
                     <p><strong>Organization:</strong> ${project.organization}</p>
                     <p>${project.description}</p>
+                    <p>current funding amount: <strong>${project.amount}</strong></p>
                     <p><strong>Rating:</strong> ${project.rating}</p>
                     <a href="#" class="donate-btn">Donate Now</a>
                 </div>
